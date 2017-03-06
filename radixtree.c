8b09f7143c5d19d111518482664f9b82d2bb075c
antirez <antirez@gmail.com>
2017-03-06 11:09:53 +0100

    Node splitting WIP.

--- radixtree.c
+++ radixtree.c
@@ -6,13 +6,14 @@
 
 void *trieNotFound = (void*)"trie-not-found-pointer";
 
-/* Allocate a new node. */
-trieNode *trieNewNode(void) {
-    trieNode *node = malloc(sizeof(*node));
+/* Allocate a new non compressed node with the specified number of children. */
+trieNode *trieNewNode(size_t children) {
+    size_t nodesize = sizeof(trieNode)+children+sizeof(trieNode*)*children;
+    trieNode *node = malloc(nodesize);
     node->iskey = 0;
     node->isnull = 0;
     node->iscompr = 0;
-    node->size = 0;
+    node->size = children;
     return node;
 }
 
@@ -21,7 +22,7 @@ trie *trieNew(void) {
     trie *trie = malloc(sizeof(*trie));
     trie->numele = 0;
     trie->numnodes = 1;
-    trie->head = trieNewNode();
+    trie->head = trieNewNode(0);
     return trie;
 }
 
@@ -102,13 +103,22 @@ trieNode *trieAddChild(trieNode *n, char c, trieNode **childptr) {
      *
      * [numc][abcd][ap][bp][cp]....|auxp|
      * [numc][abcd][ap][bp][cp][dp]|auxp| */
-    trieNode *child = trieNewNode();
+    trieNode *child = trieNewNode(0);
     n->data[n->size] = c;
     memcpy(n->data+n->size+1+sizeof(trieNode*)*n->size,
            &child,sizeof(void*));
     n->size++;
     *childptr = child;
     return n;
+}
+
+/* Return the pointer to the last child pointer in a node. For the compressed
+ * nodes this is the only child pointer. */
+trieNode **trieNodeLastChildPtr(trieNode *n) {
+    char *p = (char*)n;
+    p += trieNodeCurrentLength(n) - sizeof(trieNode*);
+    if (n->iskey && !n->isnull) p -= sizeof(void*);
+    return (trieNode**)p;
 }
 
 /* Turn the node 'n', that must be a node without any children, into a
@@ -119,20 +129,17 @@ trieNode *trieAddChild(trieNode *n, char c, trieNode **childptr) {
  * The function also returns a child node, since the last node of the
  * compressed chain cannot be part of the chain: it has zero children while
  * we can only compress inner nodes with exactly one child each. */
-trieNode *trieCompressNode(trieNode *n, char *s, size_t len, trieNode **child){
+trieNode *trieCompressNode(trieNode *n, unsigned char *s, size_t len, trieNode **child){
     assert(n->size == 0 && n->iscompr == 0);
     void *data = NULL; /* Initialized only to avoid warnings. */
-    size_t newsize, valuelen = 0;
+    size_t newsize;
 
     printf("Compress node: %.*s\n", (int)len,s);
 
     newsize = sizeof(trieNode)+len+sizeof(trieNode*);
     if (n->iskey) {
         data = trieGetData(n); /* To restore it later. */
-        if (!n->isnull) {
-            newsize += sizeof(void*);
-            valuelen = sizeof(void*);
-        }
+        if (!n->isnull) newsize += sizeof(void*);
     }
 
     n = realloc(n,newsize);
@@ -140,9 +147,8 @@ trieNode *trieCompressNode(trieNode *n, char *s, size_t len, trieNode **child){
     n->size = len;
     memcpy(n->data,s,len);
     if (n->iskey) trieSetData(n,data);
-    trieNode **childfield = (trieNode**)
-                            ((char*)n + newsize - valuelen - sizeof(trieNode*));
-    *child = trieNewNode();
+    trieNode **childfield = trieNodeLastChildPtr(n);
+    *child = trieNewNode(0);
     *childfield = *child;
     return n;
 }
@@ -151,7 +157,7 @@ trieNode *trieCompressNode(trieNode *n, char *s, size_t len, trieNode **child){
  * the pointer 'data'. If the element is already present, the associated
  * data is updated, and 0 is returned, otherwise the element is inserted
  * and 1 is returned. */
-int trieInsert(trie *trie, char *s, size_t len, void *data) {
+int trieInsert(trie *trie, unsigned char *s, size_t len, void *data) {
     size_t i = 0;
     trieNode **parentlink = NULL;
     trieNode *h = trie->head;
@@ -200,12 +206,154 @@ int trieInsert(trie *trie, char *s, size_t len, void *data) {
     }
 
     /* If the node we stopped at is a compressed node, we need to
-     * split it before to continue. */
+     * split it before to continue.
+     *
+     * Splitting a compressed node have a few possibile cases.
+     * Imagine that the node 'h' we are currently at is a compressed
+     * node contaning the string "ANNIBALE" (it means that it represents
+     * nodes A -> N -> N -> I -> B -> A -> L -> E with the only child
+     * pointer of this node pointing at the 'E' node, because remember that
+     * we have characters at the edges of the graph, not inside the nodes
+     * themselves.
+     *
+     * In order to show a real case imagine our node to also point to
+     * another compressed node, that finally points at the node without
+     * children, representing 'O':
+     *
+     *     "ANNIBALE" -> "SCO" -> []
+     *
+     * When inserting we may face the following cases. Note that all the cases
+     * require the insertion of a non compressed node with exactly two
+     * children.
+     *
+     * 1) Inserting "ANNIENTARE"
+     *
+     *               |B| -> "ALE" -> "SCO" -> []
+     *     "ANNI" -> |-|
+     *               |E| -> (... continue algo ...) "NTARE" -> []
+     *
+     * 2) Inserting "ANNIBALI"
+     *
+     *                  |E| -> "SCO" -> []
+     *     "ANNIBAL" -> |-|
+     *                  |I| -> (... continue algo ...) []
+     *
+     * 3) Inserting "AGO" (Like case 1, but set iscompr = 0 into original node)
+     *
+     *            |N| -> "NIBALE" -> "SCO" -> []
+     *     |A| -> |-|
+     *            |G| -> (... continue algo ...) |O| -> []
+     *
+     * 4) Inserting "CIAO"
+     *
+     *     |A| -> "NNIBALE" -> "SCO" -> []
+     *     |-|
+     *     |C| -> (... continue algo ...) "IAO" -> []
+     *
+     * The final algorithm for insertion covering all the above cases is as
+     * follows:
+     *
+     * 1. Save the current compressed node $NEXT pointer (the pointer to the
+     *    child element, that is always present in compressed nodes).
+     *
+     * 2. Create "split node" having as child the non common letter
+     *    at the compressed node. The other non common letter (at the key)
+     *    will be added later as we continue the normal insertion algorithm
+     *    at step "6".
+     *
+     * 3a. IF split position is 0:
+     *     Replace the old node with the split node, by copying the auxiliary
+     *     data if any. Fix parent's reference. Free old node eventually
+     *     (we still need its data for the next steps of the algorithm).
+     *
+     * 3b. IF split position != 0:
+     *     Trim the compressed node (reallocating it as well) in order to
+     *     contain $splitpos characters. Change chilid pointer in order to link
+     *     to the split node. If new compressed node len is just 1, set
+     *     iscompr to 0 (layout is the same). Fix parent's reference.
+     *
+     * 4a. IF the postfix len (the length of the remaining string of the
+     *     original compressed node after the split character) is non zero,
+     *     create a "postfix node". If the postfix node has just one character
+     *     set iscompr to 0, otherwise iscompr to 1. Set the postfix node
+     *     child pointer to $NEXT.
+     *
+     * 4b. IF the postfix len is zero, just use $NEXT as postfix pointer.
+     *
+     * 5. Set child[0] of split node to postfix node.
+     *
+     * 6. Set the split node as the current node, set current index at child[1]
+     *    and continue insertion algorithm as usually.
+     */
     if (h->iscompr) {
         printf("Stopped at compressed node %.*s\n", h->size, h->data);
         printf("Still to insert: %.*s\n", (int)(len-i), s+i);
         printf("Splitting at %d: '%c'\n", j, ((char*)h->data)[j]);
-        exit(1);
+        printf("Other letter is '%c'\n", s[i]);
+
+        /* 1: Save next pointer. */
+        trieNode **childfield = trieNodeLastChildPtr(h);
+        trieNode *next = *childfield;
+
+        /* 2: Create the split node. */
+        trieNode *splitnode = trieNewNode(1);
+        splitnode->data[0] = h->data[j];
+
+        if (j == 0) {
+            /* 3a: Replace the old node with the split node. */
+            if (h->iskey) {
+                void *ndata = trieGetData(h);
+                splitnode = trieReallocForData(splitnode,ndata);
+                trieSetData(splitnode,ndata);
+            }
+            *parentlink = splitnode;
+        } else {
+            /* 3b: Trim the compressed node. */
+            size_t nodesize = sizeof(trieNode)+j+sizeof(trieNode*);
+            if (h->iskey && !h->isnull) nodesize += sizeof(void*);
+            trieNode *trimmed = malloc(nodesize);
+            trimmed->size = j;
+            memcpy(trimmed->data,h->data,j);
+            if (h->iskey) {
+                void *ndata = trieGetData(h);
+                trieSetData(trimmed,ndata);
+            }
+            trimmed->iscompr = j > 1 ? 1 : 0;
+            trieNode **cp = trieNodeLastChildPtr(trimmed);
+            *cp = splitnode;
+            *parentlink = trimmed;
+            parentlink = cp; /* Set parentlink to splitnode parent. */
+        }
+
+        /* 4: Create the postfix node: what remains of the original
+         * compressed node after the split. */
+        size_t postfixlen = h->size - j - 1;
+        trieNode *postfix;
+        if (postfixlen) {
+            /* 4a: create a postfix node. */
+            size_t nodesize = sizeof(trieNode)+postfixlen+sizeof(trieNode*);
+            postfix = malloc(nodesize);
+            postfix->iskey = 0;
+            postfix->isnull = 0;
+            postfix->size = postfixlen;
+            postfix->iscompr = postfixlen > 1;
+            memcpy(postfix->data,h->data+j+1,postfixlen);
+            trieNode **cp = trieNodeLastChildPtr(postfix);
+            *cp = next;
+        } else {
+            /* 4b: just use next as postfix node. */
+            postfix = next;
+        }
+
+        /* 5: Set splitnode first child as the postfix node. */
+        trieNode **splitchild = trieNodeLastChildPtr(splitnode)-1;
+        *splitchild = postfix;
+
+        /* 6. Continue insertion: this will cause the splitnode to
+         * get a new child (the non common character at the currently
+         * inserted key). */
+        free(h);
+        h = splitnode;
     }
 
     /* We walked the trie as far as we could, but still there are left
@@ -247,7 +395,7 @@ int trieInsert(trie *trie, char *s, size_t len, void *data) {
 /* Find a key in the trie, returns trieNotFound special void pointer value
  * if the item was not found, otherwise the value associated with the
  * item is returned. */
-void *trieFind(trie *trie, char *s, size_t len) {
+void *trieFind(trie *trie, unsigned char *s, size_t len) {
     size_t i = 0;
     trieNode *h = trie->head;
 
@@ -364,11 +512,11 @@ int main(void) {
 int main(void) {
     printf("notfound = %p\n", trieNotFound);
     trie *t = trieNew();
-    trieInsert(t,"a",1,(void*)1);
-    printf("a = %p\n", trieFind(t,"a",1));
-    trieInsert(t,"annibale",8,(void*)2);
-    printf("a = %p\n", trieFind(t,"a",1));
-    printf("annibale = %p\n", trieFind(t,"annibale",8));
-    trieInsert(t,"annientare",10,(void*)3);
+    trieInsert(t,(unsigned char*)"a",1,(void*)1);
+    printf("a = %p\n", trieFind(t,(unsigned char*)"a",1));
+    trieInsert(t,(unsigned char*)"annibale",8,(void*)2);
+    printf("a = %p\n", trieFind(t,(unsigned char*)"a",1));
+    printf("annibale = %p\n", trieFind(t,(unsigned char*)"annibale",8));
+    trieInsert(t,(unsigned char*)"annientare",10,(void*)3);
 }
 #endif
