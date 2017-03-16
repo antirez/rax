22236734864806485750966b465ee7425f55e51a
antirez <antirez@gmail.com>
2017-03-16 09:16:06 +0100

    Benchmark with alphanumerical mode.
    For now this is a benchmark since I'm working at performance
    optimizations with nodes having many children, but this will actually
    be soon a stress tester in the test suite.

--- rax.c
+++ rax.c
@@ -1020,8 +1020,45 @@ void raxDebugShowNode(const char *msg, raxNode *n) {
 #include <stdio.h>
 #include <sys/time.h>
 
+/* This is a simple Feistel network in order to turn every possible
+ * uint32_t input into another "randomly" looking uint32_t. It is a
+ * one to one map so there are no repetitions. */
+static uint32_t int2int(uint32_t input) {
+    uint16_t l = input & 0xffff;
+    uint16_t r = input >> 16;
+    for (int i = 0; i < 8; i++) {
+        uint16_t nl = r;
+        uint16_t F = (((r * 31) + (r >> 5) + 7 * 371) ^ r) & 0xffff;
+        r = l ^ F;
+        l = nl;
+    }
+    return (r<<16)|l;
+}
+
+/* Turn an uint32_t integer into an alphanumerical key and return its
+ * length. This function is used in order to generate keys that have
+ * a large charset, so that the radix tree can be testsed with many
+ * children per node. */
+static size_t int2alphakey(char *s, size_t maxlen, uint32_t i) {
+    const char *set = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                      "abcdefghijklmnopqrstuvwxyz"
+                      "0123456789";
+    const size_t setlen = 62;
+
+    if (maxlen == 0) return 0;
+    maxlen--; /* Space for null term char. */
+    size_t len = 0;
+    while(len < maxlen) {
+        s[len++] = set[i%setlen];
+        i /= setlen;
+        if (i == 0) break;
+    }
+    s[len] = '\0';
+    return len;
+}
+
 /* Return the UNIX time in microseconds */
-long long ustime(void) {
+static long long ustime(void) {
     struct timeval tv;
     long long ust;
 
@@ -1031,64 +1068,80 @@ long long ustime(void) {
     return ust;
 }
 
+/* Turn the integer 'i' into a key according to 'mode'.
+ * mode = 0, just represents the integer as a string.
+ * mode = 1, turn it into a random-looking alphanumerical string
+ *           according to the int2alphakey() function. */
+static size_t int2key(char *s, size_t maxlen, uint32_t i, int mode) {
+    if (mode == 0) {
+        return snprintf(s,maxlen,"%lu",(unsigned long)i);
+    } else {
+        i = int2int(i);
+        return int2alphakey(s,maxlen,i);
+    }
+}
+
 int main(void) {
-    rax *t = raxNew();
-
-    long long start = ustime();
-    for (int i = 0; i < 5000000; i++) {
-        char buf[64];
-        int len = snprintf(buf,sizeof(buf),"%d",i);
-        raxInsert(t,(unsigned char*)buf,len,(void*)(long)i);
-    }
-    printf("Insert: %f\n", (double)(ustime()-start)/1000000);
-
-    start = ustime();
-    for (int i = 0; i < 5000000; i++) {
-        char buf[64];
-        int len = snprintf(buf,sizeof(buf),"%d",i);
-        void *data = raxFind(t,(unsigned char*)buf,len);
-        if (data != (void*)(long)i) {
-            printf("Issue with %s: %p instead of %p\n", buf,
-                data, (void*)(long)i);
+    for (int mode = 0; mode < 2; mode++) {
+        printf("Benchmark with %s keys:\n",
+            (mode == 0) ? "integer" : "alphanumerical");
+        rax *t = raxNew();
+        long long start = ustime();
+        for (int i = 0; i < 5000000; i++) {
+            char buf[64];
+            int len = int2key(buf,sizeof(buf),i,mode);
+            raxInsert(t,(unsigned char*)buf,len,(void*)(long)i);
         }
-    }
-    printf("Lookup: %f\n", (double)(ustime()-start)/1000000);
+        printf("Insert: %f\n", (double)(ustime()-start)/1000000);
 
-    start = ustime();
-    for (int i = 0; i < 5000000; i++) {
-        char buf[64];
-        int r = rand() % 5000000;
-        int len = snprintf(buf,sizeof(buf),"%d",r);
-        void *data = raxFind(t,(unsigned char*)buf,len);
-        if (data != (void*)(long)r) {
-            printf("Issue with %s\n", buf);
+        start = ustime();
+        for (int i = 0; i < 5000000; i++) {
+            char buf[64];
+            int len = int2key(buf,sizeof(buf),i,mode);
+            void *data = raxFind(t,(unsigned char*)buf,len);
+            if (data != (void*)(long)i) {
+                printf("Issue with %s: %p instead of %p\n", buf,
+                    data, (void*)(long)i);
+            }
         }
+        printf("Lookup: %f\n", (double)(ustime()-start)/1000000);
+
+        start = ustime();
+        for (int i = 0; i < 5000000; i++) {
+            char buf[64];
+            int r = rand() % 5000000;
+            int len = int2key(buf,sizeof(buf),r,mode);
+            void *data = raxFind(t,(unsigned char*)buf,len);
+            if (data != (void*)(long)r) {
+                printf("Issue with %s: %p instead of %p\n", buf,
+                    data, (void*)(long)r);
+            }
+        }
+        printf("Random lookup: %f\n", (double)(ustime()-start)/1000000);
+
+        start = ustime();
+        for (int i = 0; i < 5000000; i++) {
+            char buf[64];
+            int len = int2key(buf,sizeof(buf),i,mode);
+            int retval = raxRemove(t,(unsigned char*)buf,len);
+            assert(retval == 1);
+        }
+        printf("Deletion: %f\n", (double)(ustime()-start)/1000000);
+
+        start = ustime();
+        int count = 0;
+        for (int i = 0; i < 5000000; i++) {
+            char buf[64];
+            int len = int2key(buf,sizeof(buf),i,mode);
+            void *data = raxFind(t,(unsigned char*) buf,len);
+            if (data != (void*)(long)i) count++;
+        }
+        printf("Failed lookup: %f\n", (double)(ustime()-start)/1000000);
+
+        printf("%llu total nodes\n", (unsigned long long)t->numnodes);
+        printf("%llu total elements\n", (unsigned long long)t->numele);
+        raxFree(t);
     }
-    printf("Random lookup: %f\n", (double)(ustime()-start)/1000000);
-
-    start = ustime();
-    for (int i = 0; i < 5000000; i++) {
-        char buf[64];
-        int len = snprintf(buf,sizeof(buf),"%d",i);
-        int retval = raxRemove(t,(unsigned char*)buf,len);
-        assert(retval == 1);
-    }
-    printf("Deletion: %f\n", (double)(ustime()-start)/1000000);
-
-    start = ustime();
-    int count = 0;
-    for (int i = 0; i < 5000000; i++) {
-        char buf[64];
-        int len = snprintf(buf,sizeof(buf),"%d",i+5000000);
-        void *data = raxFind(t,(unsigned char*) buf,len);
-        if (data != (void*)(long)i) count++;
-    }
-    printf("Failed lookup: %f\n", (double)(ustime()-start)/1000000);
-
-    printf("%llu total nodes\n", (unsigned long long)t->numnodes);
-    printf("%llu total elements\n", (unsigned long long)t->numele);
-
-    raxFree(t);
     return 0;
 }
 #endif
